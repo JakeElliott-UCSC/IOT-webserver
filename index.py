e85c16cf8b4ff9baaf75d6b6fbd8999f32349332
Jake <jake.33.Elliott@gmail.com>
2024-06-03 20:28:22 -0700

    added webserver

---
+++ index.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return "Welcome to the simple web server!"
+
+@app.route('/data', methods=['POST'])
+def receive_data():
+    data = request.json
+    return jsonify({"message": "Data received", "data": data}), 200
+
+@app.route('/info', methods=['GET'])
+def get_info():
+    info = {
+        "name": "Simple Web Server",
+        "version": "1.0",
+        "description": "A simple web server that handles GET and POST requests."
+    }
+    return jsonify(info)
+
+if __name__ == '__main__':
+    app.run(debug=True)
